485a24b07440282435ac333e85f4c85639136aca
Leo Sin <lfsin3-c@my.cityu.edu.hk>
2019-01-30 11:46:42 +0800

    changing id from INT to VARCHAR(255)

--- dataformat.sql
+++ dataformat.sql
@@ -1,14 +1,14 @@
 # vim: ft=mysql
 
 CREATE TABLE Product (
-	id INT PRIMARY KEY AUTO_INCREMENT,
+	id VARCHAR(255) PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(255) NOT NULL,
 	description VARCHAR(255) NOT NULL,
 	weight FLOAT NOT NULL
 );
 
 CREATE TABLE Customer (
-	id INT PRIMARY KEY AUTO_INCREMENT,
+	id VARCHAR(255) PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(255) UNIQUE NOT NULL,
 	address1 VARCHAR(255) NOT NULL,
 	address2 VARCHAR(255) NOT NULL,
@@ -17,9 +17,9 @@ CREATE TABLE Customer (
 
 CREATE TABLE Request (
 	id INT PRIMARY KEY AUTO_INCREMENT,
-	Customer_id INT NOT NULL,
-	Manufacturer_id INT, -- dummy for manufacturer side
-	Product_id INT NOT NULL,
+	Customer_id VARCHAR(255) NOT NULL,
+	Manufacturer_id VARCHAR(255), -- dummy for manufacturer side
+	Product_id VARCHAR(255) NOT NULL,
 	qty INT NOT NULL,
 	order_date DATE NOT NULL,
 	due_date DATE NOT NULL,
