b2c2afd5ed287094e1c522e5fc47e6f3075b8032
Leo Sin <lfsin3-c@my.cityu.edu.hk>
2019-01-30 11:52:54 +0800

    set all id to VARCHAR(12)

--- dataformat.sql
+++ dataformat.sql
@@ -1,24 +1,24 @@
 # vim: ft=mysql
 
 CREATE TABLE Product (
-	id VARCHAR(1212) PRIMARY KEY AUTO_INCREMENT,
+	id VARCHAR(12) PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(255) NOT NULL,
 	description VARCHAR(255) NOT NULL,
 	weight FLOAT NOT NULL
 );
 
 CREATE TABLE Customer (
-	id VARCHAR(255) PRIMARY KEY AUTO_INCREMENT,
+	id VARCHAR(12) PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(255) UNIQUE NOT NULL,
 	address1 VARCHAR(255) NOT NULL,
-	address2 VARCHAR(255) NOT NULL,
-	address3 VARCHAR(255) NOT NULL
+	address2 VARCHAR(255),
+	address3 VARCHAR(255)
 );
 
 CREATE TABLE Request (
 	id INT PRIMARY KEY AUTO_INCREMENT,
-	Customer_id VARCHAR(255) NOT NULL,
-	Manufacturer_id VARCHAR(255), -- dummy for manufacturer side
+	Customer_id VARCHAR(12) NOT NULL,
+	Manufacturer_id VARCHAR(12), -- dummy for manufacturer side
 	Product_id VARCHAR(12) NOT NULL,
 	qty INT NOT NULL,
 	order_date DATE NOT NULL,
