ca95fd123ed8917debe4ef27fdb7184df8e4fda2
Leo Sin <leo69219362@gmail.com>
2019-03-27 16:40:41 +0800

    updated

--- tax_assessment/simple_tax_assessment.py
+++ tax_assessment/simple_tax_assessment.py
@@ -9,7 +9,7 @@ STANDARD_TAX_RATE = 0.15
 
 ALLOWANCE = [132000, 264000]
 
-def main(): # main function
+def get_marital_status():
     while True:
         marital_status = input("Please input your marital status. [Y/N]")
         marital_status = marital_status[0].lower()
@@ -19,39 +19,50 @@ def main(): # main function
             print ("ValueError. Please input again")
             continue
         break
+    return marital_status
 
-    division(marital_status)
 
-def division(marital_status):
+def main(): # main function
+    division()
+
+def deduction(incomes):
+    return sum(map(mpf, incomes))
+
+def division():
+    self_income = int(input("Please input your income > "))
+    print_tax("Personal", self_income - deduction([self_income]))
+
+    incomes = [self_income]
+
+    marital_status = get_marital_status()
     if marital_status:
-        self_income = int(input("Please input your income > "))
-        print ("Personal MPF is:", mpf(self_income))
-        print_tax("Personal", self_income)
-        spouse_income = int(input("Please input spouse income >"))
+        spouse_income = int(input("Please input spouse income > "))
        
-        print ("spouse MPF is:", mpf(spouse_income))
-        print_tax("Spouse", spouse_income)
-        
-        values = [s_tax(self_income) + s_tax(spouse_income),
-                  tax(self_income, marital_status) + tax(spouse_income, marital_status),
+        print_tax("Spouse", spouse_income - deduction([self_income]))
+
+        incomes.append(spouse_income)
+
+        total_income = sum(incomes)
+        joint_net_income = total_income - deduction(incomes)
+
+        values = [s_tax(joint_net_income),
+                  tax(joint_net_income, True),
                   tax(self_income, False) + tax(spouse_income, False)]
 
         index = values.index(min(values))
         case_labels = [
-            "Recommend separate assessment using standard Tax Rate",
+            "Recommend joint assessment using standard Tax Rate",
             "Recommend joint assessment using progressive Tax Rate",
             "Recommend separate assessment using progressive Tax Rate",
         ]
 
+        print_tax("Joint", joint_net_income)
+
+        print ("required tax: %.2f" % values[index])
         print (case_labels[index])
         
         
     else:
-        self_income = int(input("Please input your income > "))
-        print ("Personal MPF is:", mpf(self_income))
-        print ("Personal Tax (standard rate) is: %.2f" % s_tax(self_income))
-        print ("Personal Tax (progressive rate)is: %.2f" % tax(self_income, marital_status))
-
         values = [s_tax(self_income), tax(self_income, False)]
         index = values.index(min(values))
         case_labels = [
@@ -59,14 +70,14 @@ def division(marital_status):
             "Recommend progressive Tax Rate."
         ]
 
+        print ("required tax: %.2f" % values[index])
         print (case_labels[index])
 
     
 def print_tax(role, income):
-    print (role, "Tax (separate, standard rate) is: %.2f" % s_tax(income))
-    print (role, "Tax (separate, progressive rate) is: %.2f" % tax(income, False))
-    print (role, "Tax (joint, standard rate) is: %.2f" % s_tax(income))
-    print (role, "Tax (joint, progressive rate) is: %.2f" % tax(income, True))
+    print ("%s MPF is: %.2f" % (role, mpf(income)))
+    print ("%s Tax in standard rate: %.2f" % (role, s_tax(income)))
+    print ("%s Tax in progressive rate: %.2f" % (role, tax(income, False)))
 
 
 def mpf(income):
