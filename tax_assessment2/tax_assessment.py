a7a9908294017973bc3a2bb4bd108288382764b4
Tiffany Wong <wwytiffany1998@gmail.com>
2019-03-28 19:09:33 +0800

    not tested

--- tax_assessment2/tax_assessment.py
+++ tax_assessment2/tax_assessment.py
@@ -3,6 +3,43 @@ import sys
 if sys.version_info[0] < 3:
     input = raw_input
 
+def MPF(total_income):
+    if total_income < 7100 * 12:
+        return 0
+    return min(18000, total_income * 0.05)
+        
+def net_income(total_income):
+    """ Personal Net Income """
+    return total_income - MPF(total_income) 
+
+def NCI(net_income, combined = False):
+    allowance = 132000
+    if combined:
+        allowance *= 2
+    return net_income - allowance
+
+def standard_tax(net_income):
+    return net_income * 0.15
+
+def progressive_tax(nci):
+    if nci <= 50000:
+        return nci * 0.02
+    if nci <= 100000:
+        return 1000 + (nci - 50000) * 0.06
+    if nci <= 150000:
+        return 4000 + (nci - 100000) * 0.10
+    if nci <= 200000:
+        return 9000 + (nci - 150000) * 0.14
+
+    # Remainder
+    return 16000 + (nci - 200000) * 0.17
+
+
+def tax_selection(net_income, combined = False):
+    standard = standard_tax(net_income)
+    progressive = progressive_tax(NCI(net_income, combined))
+
+    return [standard, progressive], int(progressive < standard)
 
 def tax_calculation(data):
     """ Tax Calculator
@@ -17,12 +54,33 @@ def tax_calculation(data):
     Output Format:
         {
             "marital_status": bool,
-            "seperate": bool,
+            "combined": bool,
             "self_tax": int,
             "spouse_tax": int,
             "combined_tax": int,
         }
     """
+
+    output = {
+        "marital_status": False,
+        "combined": False,
+        "self_tax": 0,
+        "spouse_tax": 0,
+        "combined_tax": 0,
+    }
+    
+    values, choice = tax_selection(net_income(data["self_income"]))
+    output["self_tax"] = values[choice]
+
+    values, choice = tax_selection(net_income(data["spouse_income"]))
+    output["spouse_tax"] = values[choice]
+
+    values, choice = tax_selection(net_income(data["self_income"] + data["spouse_income"]), combined = True)
+    output["combined_tax"] = values[choice]
+
+    output["combined"] = output["combined_tax"]] < output["self_tax"] + output["spouse_tax"]
+
+    return output
     
 
 
@@ -52,7 +110,7 @@ def show_ouput(result):
     if result["marital_status"]:
         print ("Spouse's seperate tax payable: %d" % result["spouse_tax"])
         print ("Joint Tax payable: %d" % result["combined_tax"])
-        print ("Suggest: %s" % ("Seperating" if result["seperate"] else "Combining"))
+        print ("Suggest: %s" % ("Seperating" if not result["combined"] else "Combining"))
 
 
 
