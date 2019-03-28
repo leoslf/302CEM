8f49f03a6231e7ed6580ed46ea3a415f4e01a0fb
Leo Sin <lfsin3-c@my.cityu.edu.hk>
2019-03-29 00:56:38 +0800

    added tax_assessment3

---
+++ tax_assessment3/tests/test_tax_assessment.py
@@ -0,0 +1,18 @@
+import csv
+import pytest
+
+from tax_assessment import *
+
+def cases():
+    with open("tax_assessment_unittest.csv") as f:
+        return list(csv.DictReader(f))
+
+@pytest.mark.parametrize("case", cases())
+def test_run(case):
+    result = tax_calculation(case)
+    print (result)
+    print ("Combined Tax: %d", (result["self_tax"] + result["spouse_tax"]))
+    print ("case")
+    print (case)
+    assert int(case["expected_tax"]) == (result["combined_tax"] if result["combined"] else (result["self_tax"] + result["spouse_tax"]))
+
