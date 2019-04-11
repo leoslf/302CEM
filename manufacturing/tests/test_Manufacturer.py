ed92022f9b981c416391bac1c183841ef91ec9b4
Leo Sin <leo69219362@gmail.com>
2019-04-11 22:17:20 +0800

    updating db creation script

--- manufacturing/tests/test_Manufacturer.py
+++ manufacturing/tests/test_Manufacturer.py
@@ -5,28 +5,31 @@ import database_credential
 
 from Manufacturer import *
 
-testing_db = "302CEM_Test"
-credential = database_credential.db
+credential_testing = dict(host="localhost", user="302CEM_Test", password="302CEM_Test", db="302CEM_Test")
+
+def create_script():
+    with open("dataformat.sql") as f:
+        return f.read()
 
 def test_createdb():
-    credential.pop("db")
-    conn = pymysql.connect(**credential)
-    with conn.cursor() as cursor:
-        cursor.execute("DROP DATABASE IF EXISTS %s" % testing_db)
-        cursor.execute("CREATE DATABASE %s" % testing_db)
-        cursor.execute("SHOW DATABASES")
-        for row in cursor.fetchall():
-            print(row)
-    conn.commit()
-    conn.close()
-    credential["db"] = testing_db
+    credential = database_credential.db
+    credential.update(credential_testing)
+    assert all(credential[key] == credential_testing[key] for key in credential.keys())
+    print (create_script())
+    with pymysql.connect(**credential) as conn:
+        with conn.cursor() as cursor:
+            cursor.execute(create_script())
+            cursor.execute("SHOW TABLES")
+            for row in cursor.fetchall():
+                print(row)
     assert False
 
 def test_dropdb():
-    credential.pop("db")
-    conn = pymysql.connect(**credential)
-    with conn.cursor() as cursor:
-        cursor.execute("DROP DATABASE %s" % testing_db)
+    # credential.pop("db")
+    # conn = pymysql.connect(**credential)
+    # with conn.cursor() as cursor:
+    #     cursor.execute("DROP DATABASE %s" % testing_db)
+    pass
 
 
 # def cases():
