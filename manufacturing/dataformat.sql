586dfc2b38c008524ded34faa4c90c198b7c34df
Leo Sin <lfsin3-c@my.cityu.edu.hk>
2019-04-12 01:51:31 +0800

    fixed tests

--- manufacturing/dataformat.sql
+++ manufacturing/dataformat.sql
@@ -7,22 +7,9 @@
 -- 伺服器版本: 10.1.36-MariaDB
 -- PHP 版本： 7.3.4
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- 資料庫： 302CEM
 --
-
-
 
 -- --------------------------------------------------------
 
@@ -30,7 +17,7 @@ SET time_zone = "+00:00";
 -- 資料表結構 Customer
 --
 
-CREATE TABLE IF NOT EXISTS Customer (
+CREATE OR REPLACE TABLE Customer (
 	id varchar(12) PRIMARY KEY NOT NULL,
 	name varchar(32) UNIQUE NOT NULL,
 	address1 varchar(255) NOT NULL,
@@ -38,17 +25,8 @@ CREATE TABLE IF NOT EXISTS Customer (
 	address3 varchar(255) DEFAULT NULL,
 	email varchar(255) DEFAULT 'name@email.com',
 	contact varchar(8) DEFAULT '98765432'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+);
 
---
--- 資料表的匯出資料 Customer
---
-
-INSERT IGNORE INTO Customer (id, name, address1, address2, address3, email, contact) VALUES
-('', 'DUMMY', 'DUMMY Building, -1 DUMMY Street', 'DUMMY District ', 'DUMMY', 'name@email.com', '98765432'),
-('000000000001', 'Web Store 1', 'NO.45, WO HING TSUEN,NO. 45, WO HING TSUEN', 'NORTH', 'NT', 'name@email.com', '98765432'),
-('000000000002', 'Web Store 2', 'BLOCK A23,EUROPA GARDEN,NO. 48,KWU TUNG ROAD', 'NORTH', 'NT', 'name@email.com', '98765432'),
-('000000000003', 'Web Store 3', '338, FAN KAM ROAD, The Green , Maple Drive F2', 'NORTH', 'NT', 'name@email.com', '98765432');
 
 -- --------------------------------------------------------
 
@@ -58,33 +36,27 @@ INSERT IGNORE INTO Customer (id, name, address1, address2, address3, email, cont
 -- 資料表結構 Logistics
 --
 
-CREATE TABLE IF NOT EXISTS Logistics (
+CREATE OR REPLACE TABLE Logistics (
 	id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
 	name varchar(64) UNIQUE NOT NULL,
 	address1 varchar(255) NOT NULL,
 	address2 varchar(255) DEFAULT NULL,
 	address3 varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+);
 
-
-INSERT IGNORE INTO Logistics (id, name, address1, address2, address3) VALUES
-(-1, 'DUMMY Logistics Company', 'DUMMY Address', NULL, NULL),
-(1, 'Logistics Company 1', 'Logistics Company 1 Address', NULL, NULL),
-(2, 'Logistics Company 2', 'Logistics Company 2 Company', NULL, NULL),
-(3, 'Logistics Company 3', 'Logistics Company 3 Address', NULL, NULL);
 -- --------------------------------------------------------
 
 --
 -- 資料表結構 Logistics_Request
 --
 
-CREATE TABLE IF NOT EXISTS Logistics_Request (
+CREATE OR REPLACE TABLE Logistics_Request (
 	id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
 	quantity int(11) NOT NULL,
 	weight float NOT NULL,
 	Customer_id varchar(12) NOT NULL,
 	FOREIGN KEY (Customer_id) REFERENCES Customer (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+);
 
 -- --------------------------------------------------------
 
@@ -92,23 +64,12 @@ CREATE TABLE IF NOT EXISTS Logistics_Request (
 -- 資料表結構 Product
 --
 
-CREATE TABLE IF NOT EXISTS Product (
+CREATE OR REPLACE TABLE Product (
 	id varchar(12) PRIMARY KEY NOT NULL,
 	name varchar(255) NOT NULL,
 	description varchar(255) NOT NULL,
 	weight float NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- 資料表的匯出資料 Product
---
-
-INSERT IGNORE INTO Product (id, name, description, weight) VALUES
-('', 'DUMMY', 'Dummy to occupy the empty string ID', 0),
-('000000000001', 'Custom Product 1', 'Test Product', 1),
-('000000000002', 'Custom Product 2', 'Test Product 2', 2),
-('000000000003', 'Custom Product 3', 'Test Product 3', 3);
-
+);
 
 -- --------------------------------------------------------
 
@@ -116,7 +77,7 @@ INSERT IGNORE INTO Product (id, name, description, weight) VALUES
 -- 資料表結構 Request
 --
 
-CREATE TABLE IF NOT EXISTS Request (
+CREATE OR REPLACE TABLE Request (
 	id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
 	Customer_id varchar(12) NOT NULL,
 	Manufacturer_id varchar(12) DEFAULT NULL,
@@ -127,22 +88,7 @@ CREATE TABLE IF NOT EXISTS Request (
 	Invoice_id int(11) NOT NULL,
 	FOREIGN KEY (Customer_id) REFERENCES Customer (id),
 	FOREIGN KEY (Product_id) REFERENCES Product (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- 資料表的匯出資料 Request
---
-
-INSERT IGNORE INTO Request (id, Customer_id, Manufacturer_id, Product_id, qty, order_date, due_date, Invoice_id) VALUES
-(3, '000000000002', '012', '10', 10, '0000-00-00', '0000-00-00', 413),
-(4, '000000000002', '012', '10', 10, '0000-00-00', '0000-00-00', 413),
-(5, '000000000002', '012', '10', 10, '0000-00-00', '0000-00-00', 413),
-(6, '000000000002', '012', '10', 10, '0000-00-00', '0000-00-00', 413),
-(7, '000000000002', '012', '10', 10, '0000-00-00', '0000-00-00', 413),
-(8, '000000000002', '012', '10', 10, '0000-00-00', '0000-00-00', 413),
-(9, '000000000002', '012', '10', 10, '0000-00-00', '0000-00-00', 413),
-(10, '000000000002', '012', '10', 10, '0000-00-00', '0000-00-00', 413);
-
+);
 
 -- --------------------------------------------------------
 
@@ -150,13 +96,13 @@ INSERT IGNORE INTO Request (id, Customer_id, Manufacturer_id, Product_id, qty, o
 -- 資料表結構 Logistics_Request_Request
 --
 
-CREATE TABLE IF NOT EXISTS Logistics_Request_Request (
+CREATE OR REPLACE TABLE Logistics_Request_Request (
 	Logistics_Request_id int(11) NOT NULL,
 	Request_id int(11) NOT NULL,
 	PRIMARY KEY (Logistics_Request_id, Request_id),
 	FOREIGN KEY (Logistics_Request_id) REFERENCES Logistics_Request (id),
 	FOREIGN KEY (Request_id) REFERENCES Request (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+);
 
 -- --------------------------------------------------------
 
@@ -166,17 +112,14 @@ CREATE TABLE IF NOT EXISTS Logistics_Request_Request (
 -- 資料表結構 Material
 --
 
-CREATE TABLE IF NOT EXISTS Material (
+CREATE OR REPLACE TABLE Material (
 	id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
 	name varchar(64) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+);
 
 --
 -- 資料表的匯出資料 Material
 --
-
-INSERT IGNORE INTO Material (id, name) VALUES
-(1, 'Test Material');
 
 -- --------------------------------------------------------
 
@@ -184,7 +127,7 @@ INSERT IGNORE INTO Material (id, name) VALUES
 -- 資料表結構 Production
 --
 
-CREATE TABLE IF NOT EXISTS Production (
+CREATE OR REPLACE TABLE Production (
 	id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
 	Product_id varchar(12) NOT NULL,
 	qty int(11) NOT NULL,
@@ -192,7 +135,7 @@ CREATE TABLE IF NOT EXISTS Production (
 	Request_id int(11) NOT NULL,
 	FOREIGN KEY (Product_id) REFERENCES Product (id),
 	FOREIGN KEY (Request_id) REFERENCES Request (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+);
 
 -- --------------------------------------------------------
 
@@ -200,7 +143,7 @@ CREATE TABLE IF NOT EXISTS Production (
 -- 資料表結構 Consumption
 --
 
-CREATE TABLE IF NOT EXISTS Consumption (
+CREATE OR REPLACE TABLE Consumption (
 	Production_id int(11) NOT NULL,
 	Material_id int(11) NOT NULL,
 	qty int(11) NOT NULL,
@@ -208,7 +151,7 @@ CREATE TABLE IF NOT EXISTS Consumption (
 	PRIMARY KEY (Production_id, Material_id),
 	FOREIGN KEY (Production_id) REFERENCES Production (id),
 	FOREIGN KEY (Material_id) REFERENCES Material (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+);
 
 -- --------------------------------------------------------
 
@@ -216,21 +159,14 @@ CREATE TABLE IF NOT EXISTS Consumption (
 -- 資料表結構 Recipe
 --
 
-CREATE TABLE IF NOT EXISTS Recipe (
+CREATE OR REPLACE TABLE Recipe (
 	Product_id varchar(12) NOT NULL,
 	Material_id int(11) NOT NULL,
 	qty int(11) NOT NULL,
 	PRIMARY KEY (Product_id,Material_id),
 	FOREIGN KEY (Product_id) REFERENCES Product (id),
 	FOREIGN KEY (Material_id) REFERENCES Material (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- 資料表的匯出資料 Recipe
---
-
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES
-('10', 1, 1000);
+);
 
 -- --------------------------------------------------------
 
@@ -241,13 +177,13 @@ INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES
 -- 資料表結構 Restock
 --
 
-CREATE TABLE IF NOT EXISTS Restock (
+CREATE OR REPLACE TABLE Restock (
 	id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
 	Material_id int(11) NOT NULL,
 	qty int(11) NOT NULL,
 	create_timestamp timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	FOREIGN KEY (Material_id) REFERENCES Material (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+);
 
 
 -- --------------------------------------------------------
@@ -322,106 +258,3 @@ ON
 -- --------------------------------------------------------
 
 
-
-
-
-COMMIT;
-
-START TRANSACTION;
-
-INSERT IGNORE INTO Customer (id, `name`, address1, address2, address3, email, contact) VALUES('', 'DUMMY', 'DUMMY Building, -1 DUMMY Street', 'DUMMY District ', 'DUMMY', 'name@email.com', '98765432');
-INSERT IGNORE INTO Customer (id, `name`, address1, address2, address3, email, contact) VALUES('000000000001', 'Web Store 1', 'NO.45, WO HING TSUEN,NO. 45, WO HING TSUEN', 'NORTH', 'NT', 'name@email.com', '98765432');
-INSERT IGNORE INTO Customer (id, `name`, address1, address2, address3, email, contact) VALUES('000000000002', 'Web Store 2', 'BLOCK A23,EUROPA GARDEN,NO. 48,KWU TUNG ROAD', 'NORTH', 'NT', 'name@email.com', '98765432');
-INSERT IGNORE INTO Customer (id, `name`, address1, address2, address3, email, contact) VALUES('000000000003', 'Web Store 3', '338, FAN KAM ROAD, The Green , Maple Drive F2', 'NORTH', 'NT', 'name@email.com', '98765432');
-INSERT IGNORE INTO Logistics (id, `name`, address1, address2, address3) VALUES(-1, 'DUMMY Logistics Company', 'DUMMY Address', NULL, NULL);
-INSERT IGNORE INTO Logistics (id, `name`, address1, address2, address3) VALUES(1, 'Logistics Company 1', 'Logistics Company 1 Address', NULL, NULL);
-INSERT IGNORE INTO Logistics (id, `name`, address1, address2, address3) VALUES(2, 'Logistics Company 2', 'Logistics Company 2 Company', NULL, NULL);
-INSERT IGNORE INTO Logistics (id, `name`, address1, address2, address3) VALUES(3, 'Logistics Company 3', 'Logistics Company 3 Address', NULL, NULL);
-INSERT IGNORE INTO Material (id, `name`) VALUES(1, 'Test Material');
-INSERT IGNORE INTO Material (id, `name`) VALUES(2, 'Test Material 2');
-INSERT IGNORE INTO Material (id, `name`) VALUES(3, 'Test Material 3');
-INSERT IGNORE INTO Material (id, `name`) VALUES(4, 'Test Material 4');
-INSERT IGNORE INTO Material (id, `name`) VALUES(5, 'Test Material 5');
-INSERT IGNORE INTO Material (id, `name`) VALUES(6, 'Test Material 6');
-INSERT IGNORE INTO Material (id, `name`) VALUES(7, 'Test Material 7');
-INSERT IGNORE INTO Material (id, `name`) VALUES(8, 'Test Material 8');
-INSERT IGNORE INTO Material (id, `name`) VALUES(9, 'Test Material 9');
-INSERT IGNORE INTO Material (id, `name`) VALUES(10, 'Test Material 10');
-INSERT IGNORE INTO Product (id, `name`, description, weight) VALUES('', 'DUMMY', 'Dummy to occupy the empty string ID', 0);
-INSERT IGNORE INTO Product (id, `name`, description, weight) VALUES('000000000001', 'Custom Product 1', 'Test Product', 1);
-INSERT IGNORE INTO Product (id, `name`, description, weight) VALUES('000000000002', 'Custom Product 2', 'Test Product 2', 2);
-INSERT IGNORE INTO Product (id, `name`, description, weight) VALUES('000000000003', 'Custom Product 3', 'Test Product 3', 3);
-INSERT IGNORE INTO Product (id, `name`, description, weight) VALUES('000000000004', 'Custom Product 4', 'Test Product 4', 4.5);
-INSERT IGNORE INTO Product (id, `name`, description, weight) VALUES('000000000005', 'Custom Product 5', 'Test Product 5', 5);
-INSERT IGNORE INTO Product (id, `name`, description, weight) VALUES('000000000006', 'Custom Product 6', 'Test Product 6', 6.5);
-INSERT IGNORE INTO Product (id, `name`, description, weight) VALUES('000000000007', 'Custom Product 7', 'Test Product 7', 7);
-INSERT IGNORE INTO Product (id, `name`, description, weight) VALUES('000000000008', 'Custom Product 8', 'Test Product 8', 8.5);
-INSERT IGNORE INTO Product (id, `name`, description, weight) VALUES('000000000009', 'Custom Product 9', 'Test Product 9', 9);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000001', 1, 7);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000001', 4, 10);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000001', 5, 7);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000001', 7, 9);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000001', 8, 5);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000001', 9, 1);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000001', 10, 10);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000002', 3, 1);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000002', 4, 4);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000002', 6, 1);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000002', 9, 1);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000003', 2, 2);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000003', 3, 2);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000003', 4, 3);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000003', 5, 10);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000003', 6, 2);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000003', 7, 11);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000003', 8, 4);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000003', 9, 1);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000003', 10, 3);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000004', 2, 3);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000004', 4, 9);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000004', 5, 4);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000004', 7, 11);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000004', 9, 5);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000004', 10, 9);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000005', 1, 3);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000005', 4, 5);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000005', 5, 8);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000005', 6, 5);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000005', 7, 4);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000005', 8, 6);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000005', 9, 8);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000005', 10, 3);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000006', 1, 8);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000006', 2, 8);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000006', 3, 1);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000006', 7, 8);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000007', 1, 4);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000007', 2, 7);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000007', 3, 2);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000007', 4, 11);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000007', 5, 2);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000007', 6, 7);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000007', 7, 2);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000007', 8, 8);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000007', 9, 4);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000007', 10, 7);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000008', 1, 5);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000008', 5, 6);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000008', 6, 1);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000009', 5, 7);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000009', 7, 10);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000009', 9, 7);
-INSERT IGNORE INTO Recipe (Product_id, Material_id, qty) VALUES('000000000009', 10, 5);
-INSERT IGNORE INTO Request (id, Customer_id, Manufacturer_id, Product_id, qty, order_date, due_date, Invoice_id) VALUES(3, '000000000002', '012', '10', 10, '0000-00-00', '0000-00-00', 413);
-INSERT IGNORE INTO Request (id, Customer_id, Manufacturer_id, Product_id, qty, order_date, due_date, Invoice_id) VALUES(4, '000000000002', '012', '10', 10, '0000-00-00', '0000-00-00', 413);
-INSERT IGNORE INTO Request (id, Customer_id, Manufacturer_id, Product_id, qty, order_date, due_date, Invoice_id) VALUES(5, '000000000002', '012', '10', 10, '0000-00-00', '0000-00-00', 413);
-INSERT IGNORE INTO Request (id, Customer_id, Manufacturer_id, Product_id, qty, order_date, due_date, Invoice_id) VALUES(6, '000000000002', '012', '10', 10, '0000-00-00', '0000-00-00', 413);
-INSERT IGNORE INTO Request (id, Customer_id, Manufacturer_id, Product_id, qty, order_date, due_date, Invoice_id) VALUES(7, '000000000002', '012', '10', 10, '0000-00-00', '0000-00-00', 413);
-INSERT IGNORE INTO Request (id, Customer_id, Manufacturer_id, Product_id, qty, order_date, due_date, Invoice_id) VALUES(8, '000000000002', '012', '10', 10, '0000-00-00', '0000-00-00', 413);
-INSERT IGNORE INTO Request (id, Customer_id, Manufacturer_id, Product_id, qty, order_date, due_date, Invoice_id) VALUES(9, '000000000002', '012', '10', 10, '0000-00-00', '0000-00-00', 413);
-INSERT IGNORE INTO Request (id, Customer_id, Manufacturer_id, Product_id, qty, order_date, due_date, Invoice_id) VALUES(10, '000000000002', '012', '10', 10, '0000-00-00', '0000-00-00', 413);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- manufacturing/tests/test_Manufacturer.py
+++ manufacturing/tests/test_Manufacturer.py
@@ -3,6 +3,7 @@ import csv
 import pytest
 import pymysql
 import database_credential
+import operator
 
 from Manufacturer import *
 from db_connection import OrderedDictCursor
@@ -10,39 +11,64 @@ from db_connection import OrderedDictCursor
 credential_testing = dict(host="localhost", user="302CEM_Test", password="302CEM_Test", db="302CEM_Test")
 
 expected_tables = [
-    ('Consumption',),
-    ('Consumption_View',),
-    ('Customer',),
-    ('Inventory',),
-    ('Logistics',),
-    ('Logistics_Request',),
-    ('Logistics_Request_Request',),
-    ('Logistics_Request_View',),
-    ('Material',),
-    ('Product',),
-    ('Production',),
-    ('Recipe',),
-    ('Request',),
-    ('Request_View',),
-    ('Restock',),
-    ('Restock_View',)
+    'Consumption',
+    'Customer',
+    'Logistics',
+    'Logistics_Request',
+    'Logistics_Request_Request',
+    'Material',
+    'Product',
+    'Production',
+    'Recipe',
+    'Request',
+    'Restock',
+    'Restock_View',
+    'Request_View',
+    'Inventory',
+    'Consumption_View',
+    'Logistics_Request_View',
 ]
+
 
 @pytest.fixture
 def create_script():
     with open("dataformat.sql") as f:
         return f.read()
 
-def test_setupdb(create_script):
+@pytest.fixture
+def insert_script():
+    with open("data.sql") as f:
+        return f.read()
+
+def test_setupdb(create_script, insert_script):
     credential = database_credential.db
     credential.update(credential_testing)
     assert all(credential[key] == credential_testing[key] for key in credential.keys())
+
     conn = pymysql.connect(**credential)
     with conn.cursor() as cursor:
         cursor.mogrify(create_script)
+    conn.commit()
+    conn.close()
+
+    conn = pymysql.connect(**credential)
+    with conn.cursor() as cursor:
         cursor.execute("SHOW TABLES")
-        actual_tables = list(cursor.fetchall())
-        assert actual_tables == expected_tables
+        actual_tables = cursor.fetchall()
+        print (set(expected_tables))
+        actual_tables = list(map(operator.itemgetter(0), actual_tables))
+        print (set(actual_tables))
+        assert set(map(lambda s: s.lower(), actual_tables)) == set(map(lambda s: s.lower(), expected_tables))
+
+    with conn.cursor() as cursor:
+        cursor.execute("SHOW FULL TABLES WHERE Table_Type != 'VIEW'")
+        for table in map(operator.itemgetter(0), cursor.fetchall()):
+            cursor.execute("DELETE FROM %s" % table)
+    conn.commit()
+    
+    with conn.cursor() as cursor:
+        cursor.mogrify(insert_script)
+        
     conn.commit()
     conn.close()
 
